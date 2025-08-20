696a527a6f6a43f41aa0c26b10c770570902318c
arnavmalhotra2025 <arnav.malhotra2025@gmail.com>
2025-08-20 16:20:36 +0200

    Add files via upload

---
+++ project frecuency.py
@@ -0,0 +1,12 @@
+test_dic = {'codingal':3, 'is':2, 'best':2, 'for':2, 'Coding':1}
+frecuency = int(input("please enter the number you want to know the frcueny of:"))
+
+if frecuency == 3:
+    print("1")
+elif frecuency == 2:
+    print("3")
+elif frecuency == 1:
+    print("1")
+else:
+    print("0")
+    
